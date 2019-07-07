4e0155491dd1ad955eab49957053d394a90a1cf8
Logan D.C. Bishop <ldcbishop@gmail.com>
2019-07-06 22:04:03 -0500

    Adding noisy/denoised images
    Adding the denoised images to the notebook.

--- imageProc.py
+++ imageProc.py
@@ -1,183 +1,126 @@
-#%%
+def nm_to_rgb(nm):
+	"""
+	Accepts a wavelength value in either string format (such as those provided by 'input()') or direct numerical format (e.g. 584) and converts it to the related RGB values.  Based on the code given at:
+		http://www.efg2.com/Lab/ScienceAndEngineering/Spectra.htm
+
+	---------------------------------------------------------------------------------------
+	Arument Definitions:
+	> nm:   (#)     A numerical value for the wavelength to convert
+			("str") A string value for the wavelength to convert
+
+	---------------------------------------------------------------------------------------
+	Output:
+	< rgb:  (r, g, b)   A byte tuple containing the red, green, and blue values for 'nm_str'
+	"""
+
+	## Adjustment function ##
+	def Adjust(color, intensity):
+		"""
+		This does some fancy thing that they talk about on the website.  I'm not really sure what it is or does, but here it is.  At the very least we want to avoid 0^x = 1 cases, so there's that.
+		"""
+		# Constants #
+		GAMMA = 0.80
+		INTENSITY_MAX = 255
+
+		# Don't want 0^x = 1 for x <> 0 #
+		if(color == 0.0):
+			return 0
+		else:
+			# This is adjusting/normalizing the color/intensity into a linear value.
+			return round(INTENSITY_MAX * pow(color * intensity, GAMMA))
+
+	## Initialization ##
+	# Prime the tuple with the R G B values set to 0, we'll combine them later #
+	r = 0
+	g = 0
+	b = 0
+
+	# Also introduce the intensity factor for dimming towards the ends of the spectrum. #
+	intensity = 0
+
+	## Determine RGB Values ##
+	# Determine if the input value is a string or a numerical value #
+	if(type(nm) == str):
+		# Parse the string and overwrite the value #
+		nm = float(nm)
+
+	# These values won't change, so we can hard-code them #
+	if( (380 <= nm) and (nm < 440) ):
+		# The color fades from purple to blue #
+		r = -(nm - 440)/(440-380)
+		b = 1
+	elif( (440 <= nm) and (nm < 490) ):
+		# The color fades from blue to cyan #
+		g = (nm - 440)/(490-440)
+		b = 1
+	elif( (490 <= nm) and (nm < 510) ):
+		# The color fades from cyan to green #
+		g = 1
+		b = -(nm - 510)/(510-490)
+	elif( (510 <= nm) and (nm < 580) ):
+		# The color fades from green to yellow #
+		r = (nm - 510)/(580-510)
+		g = 1
+	elif( (580 <= nm) and (nm < 645) ):
+		# The color fades from yellow to red #
+		r = 1
+		g = -(nm - 645)/(645-580)
+	elif( (645 <= nm) and (nm < 780) ):
+		# The color stays red for a while #
+		r = 1
+
+	## Intensity Adjustment ##
+	# Let the intensity fall off near the vision limits #
+	if( (380 <= nm) and (nm < 420) ):
+		intensity = 0.3 + 0.7*(nm-380)/(420-380)
+	elif( (420 <= nm) and (nm < 700) ):
+		intensity = 1.0
+	elif( (700 <= nm) and (nm < 780) ):
+		intensity = 0.3 - 0.7*(nm-780)/(780-700)
+
+	# Further adjust the intensity #
+	rgb = ( Adjust(r, intensity), Adjust(g, intensity), Adjust(b, intensity) )
+
+	## Output ##
+	return rgb
+	
 import numpy as np
-# ------------------------------------------------------------------------------------------------ #
-def nm2rgb(np_nm):
-    ## CONSTANTS ##
-    BNDS_NM = [380, 440, 490, 510, 580, 645, 780]
-    dBNDS_NM = np.diff(BNDS_NM, 1)
+import os
 
-    BNDS_INT = [340, 420, 700, 780]
-    dBNDS_INT = np.diff(BNDS_INT, 1)
+def noisy(noise_typ,image):
+   	if noise_typ == "gauss":
+	  	row,col,ch= image.shape
+	  	mean = 0
+	  	var = 0.1
+	  	sigma = var**0.5
+	 	gauss = np.random.normal(mean,sigma,(row,col,ch))
+	 	gauss = gauss.reshape(row,col,ch)
+	 	noisy = image + gauss
+	  	return noisy
+	elif noise_typ == "s&p":
+	  	row,col,ch = image.shape
+	  	s_vs_p = 0.5
+	  	amount = 0.004
+	  	out = np.copy(image)
+	  	# Salt mode
+	  	num_salt = np.ceil(amount * image.size * s_vs_p)
+	  	coords = [np.random.randint(0, i - 1, int(num_salt))
+			  for i in image.shape]
+	  	out[coords] = 1
 
-    SWATCH = [  [1/dBNDS_NM[0], 0, np.inf],
-                [0, 1/dBNDS_NM[1], np.inf],
-                [0, np.inf, 1/dBNDS_NM[2]],
-                [1/dBNDS_NM[3], np.inf, 0],
-                [np.inf, 1/dBNDS_NM[4], 0],
-                [np.inf, 0, 0]                  ]
-    COL_VAL = [ BNDS_NM[1], BNDS_NM[1], BNDS_NM[3], BNDS_NM[3], BNDS_NM[5], BNDS_NM[5] ]
-    POLARITY = [-1, +1, -1, +1, -1, +1]
-
-    ## Initialization ##
-    rgb = np.zeros((len(np_nm), 3))
-    Int = np.ones((len(np_nm), 1))
-
-    # Assign each color #
-    for l in range(len(np_nm)):
-        # RGB Values #
-        bin_nm = np.logical_and(np_nm[l] < BNDS_NM[1:], np_nm[l] >= BNDS_NM[:-1])
-        for b in range(0, len(BNDS_NM)):
-            if(bin_nm[b]):
-                for c in range(3):
-                    val = POLARITY[b] * (np_nm[l] - COL_VAL[b]) * SWATCH[b][c]
-                    if(np.isnan(val)):
-                        rgb[l,c] = 1
-                    else:
-                        rgb[l,c] = min(max(val, 0), 1)
-                break
-        
-        # Intensity #
-        if(BNDS_INT[0] <= np_nm[l] and np_nm[l] < BNDS_INT[1]):
-            Int[l] = +(np_nm[l] - BNDS_INT[0])/dBNDS_INT[0]
-        elif(BNDS_INT[2] <= np_nm[l] and np_nm[l] < BNDS_INT[3]):
-            Int[l] = -(np_nm[l] - BNDS_INT[3])/dBNDS_INT[2]
-
-    # Adjust values #
-    rgb = Adjust(rgb, Int)
-    return rgb
-def rgb2nm(np_rgb):
-    ## CONSTANTS ##
-    INT_MAX = 255.0
-    INT_TOL = 0.01  # Fraction of INT_MAX #
-
-    ## Initialize ##
-    nm = np.zeros(int(np.size(np_rgb)/3))
-    rgb = np_rgb / INT_MAX
-
-    if(len(nm) == 1):
-        rgb_max = np.amax(rgb)
-    else:
-        rgb_max = np.amax(rgb, axis = 1)
-
-    ## Find Wavelengths ##
-    if(len(nm) == 1):
-        # RED #
-        if(rgb[0] > (1-INT_TOL)):                   # 780 to 580 #
-            if(rgb[1] < (1-INT_TOL)):                   # 645 to 580 #
-                nm = (1-rgb[1])*(645-580) + 580
-            else:
-                nm = 580
-        # GREEN #
-        elif(rgb[1] > (1-INT_TOL)):             # 580 to 490 #
-            if(rgb[0] > INT_TOL):                       # 580 to 510 #
-                nm = (0+rgb[0])*(580-510) + 510
-            elif(rgb[2] > INT_TOL):                 # 510 to 490 #
-                nm = (1-rgb[2])*(510-490) + 490
-            else:
-                nm = 510
-        # BLUE #
-        elif(rgb[2] > rgb_max*(1-INT_TOL)): # 490 to 380 #
-            if(rgb[1] > INT_TOL):                       # 490 to 440 #
-                nm = (0+rgb[1])*(490-440) + 440
-            elif(rgb[0] > INT_TOL):                 # 440 to 380 #
-                nm = rgb_max*(1-rgb[0])*(440-380) + 380
-            else:
-                nm = 440
-    else:   
-        for c in range(len(nm)):
-            # RED #
-            if(rgb[c,0] > (1-INT_TOL)):                 # 780 to 580 #
-                if(rgb[c,1] < (1-INT_TOL)):                 # 645 to 580 #
-                    nm[c] = (1-rgb[c,1])*(645-580) + 580
-                else:
-                    nm[c] = 580
-            # GREEN #
-            elif(rgb[c,1] > (1-INT_TOL)):               # 580 to 490 #
-                if(rgb[c,0] > INT_TOL):                     # 580 to 510 #
-                    nm[c] = (0+rgb[c,0])*(580-510) + 510
-                elif(rgb[c,2] > INT_TOL):                   # 510 to 490 #
-                    nm[c] = (1-rgb[c,2])*(510-490) + 490
-                else:
-                    nm[c] = 510
-            # BLUE #
-            elif(rgb[c,2] > rgb_max[c]*(1-INT_TOL)):    # 490 to 380 #
-                if(rgb[c,1] > INT_TOL):                     # 490 to 440 #
-                    nm[c] = (0+rgb[c,1])*(490-440) + 440
-                elif(rgb[c,0] > INT_TOL):                   # 440 to 380 #
-                    nm[c] = rgb_max[c]*(1-rgb[c,0])*(440-380) + 380
-                else:
-                    nm[c] = 440
-
-    return nm
-# ------------------------------------------------------------------------------------------------ #
-def rgb2wsl(rgb):
-    ## CONSTANTS ##
-    INT_MAX = 255
-    ERR_TOL = 0.00001
-
-    print(rgb)
-
-    # Extrast WSL #
-    w = min(rgb)
-    s = max(abs(rgb - w))
-    l = rgb2nm( (rgb - w)/s * INT_MAX)
-
-    print([w, s, l])
-    print(nm2rgb(np.array([l]))[0])
-
-    # Evaluate error #
-    err = rgb - wsl2rgb( np.array([w,s,l]) )
-
-    # Correct for negative errors #
-    i = 1
-    while((err < 0).any()):
-        i = i+1
-        derr = min(err[0]) / max(nm2rgb(np.array([l]))[0])
-
-        s = s + i*derr
-        err = rgb - wsl2rgb( np.array([w,s,l]) )
-
-    ## KEEP ADDING NEW ONES UNTIL WE SATISFY THE TOLERANCE ##
-    while(sum(abs(err[0])) > ERR_TOL):
-        rgb_new = err[0]
-
-        w_new = min(rgb_new)
-        s_new = max(abs(rgb_new - w_new))
-        l_new = rgb2nm( (rgb_new - w_new)/s_new * INT_MAX)
-
-        w = np.append(w, w_new)
-        s = np.append(s, s_new)
-        l = np.append(l, l_new)
-
-        fit = np.zeros((1,3))
-        for c in range(len(w)):
-            fit += wsl2rgb( np.array([w[c], s[c], l[c]]) )
-            
-        err = rgb - fit
-        while((err < 0).any()):
-            i = i+1
-            derr = min(err[0]) / max(nm2rgb(np.array([l[-1]]))[0])
-
-            s = s + i*derr
-            fit = np.zeros((1,3))
-            for c in range(len(w)):
-                fit += wsl2rgb( np.array([w[c], s[c], l[c]]) )
-            err = rgb - fit
-
-    return np.array([w,s,l])
-def wsl2rgb(wsl):
-    ## CONSTANTS ##
-    INT_MAX = 255
-
-    # Compute the RGB value #
-    rgb = (nm2rgb([wsl[2]]) / INT_MAX) * [wsl[1]] + [wsl[0]]
-    return rgb
-# ------------------------------------------------------------------------------------------------ #
-def Adjust(color, intensity):
-    ## CONSTANTS ##
-    GAMMA = 0.80
-    INT_MAX = 255
-
-    ## Procedure ##
-    return np.round(INT_MAX * np.power(color * intensity, GAMMA))
-# ------------------------------------------------------------------------------------------------ #
+	  	# Pepper mode
+	  	num_pepper = np.ceil(amount* image.size * (1. - s_vs_p))
+	  	coords = [np.random.randint(0, i - 1, int(num_pepper)) for i in image.shape]
+	  	out[coords] = 0
+	  	return out
+  	elif noise_typ == "poisson":
+	  	vals = len(np.unique(image))
+	  	vals = 2 ** np.ceil(np.log2(vals))
+	  	noisy = np.random.poisson(image * vals) / float(vals)
+	  	return noisy
+	elif noise_typ =="speckle":
+	  	row,col,ch = image.shape
+	  	gauss = np.random.randn(row,col,ch)
+	 	gauss = gauss.reshape(row,col,ch)        
+	  	noisy = image + image * gauss
+	  	return noisy
